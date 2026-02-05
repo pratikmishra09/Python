26e6e0beb72d1f9fde8f6e31f8ad229cb485d2c1
Pratik Mishra <pk3057765@gmail.com>
2026-02-05 11:42:37 +0530

    Add Day5 functions

---
+++ Day5/functions.py
@@ -0,0 +1,32 @@
+# def sumofNumber(a , b):
+#    total = a+b
+#    return total
+# result = sumofNumber(10 , 20)
+
+# print(result)
+
+
+# Even Odd function Creating :
+
+
+# def Even_Odd( num ):
+#     if(num%2==0):
+#         "Even"
+#         return "Even"
+#     else: 
+#        "Odd"
+#     return "Odd"
+# num = Even_Odd(5)
+# print(num)
+
+
+# def add(a,b):
+# #     return a * b
+
+# # num = add(6,5)
+# # print(num)
+
+# def greet(name="Guest"):
+#   return greet
+
+#  print("Hello", name)
