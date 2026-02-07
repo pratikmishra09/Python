ebeae2ca30edfb7947a1947d166bde8f27182f14
PRATIK MISHRA <158600792+pratikmishra09@users.noreply.github.com>
2026-02-07 12:02:10 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,125 @@
+
+# # if( True):
+# #    print("X")
+# #    print("Y")
+# # else : print()
+
+# # print("X")
+# # if True: print("Z")
+# # if False: print("Z")
+
+# # """Write a Python program that outputs exactly"""
+# # print("1")
+# # if True: print("3")
+# # if False: print("2")
+
+# # print("1")
+# # print("3")
+# # exit()
+# # print()
+
+
+# # print("START")
+# # print("END")
+# # exit()
+# # print()
+# # print()
+
+
+
+# # print("BEGIN")
+# # if True: 
+# #     print("DONE")
+# # print()
+# # exit()
+# # print()
+
+# # print("Python")
+# # print("Rules")
+
+# # print("A")
+# # print("B")
+# # print("C")
+
+# # print("Hello")
+# # print("World")
+
+
+# # print("Start")
+# # if True:
+# #     print("End")
+
+
+# # print("X")
+# # if True:print("Y")
+
+
+# # if True:print("Done")
+
+# # if True:
+# #  print("Hi")
+# # print("There")
+
+
+# # if True:
+# #     print("A")
+# #     print("B")
+
+
+
+# # print("Start")
+# # if True:
+# #     print("Middle")
+# #     print("End")
+
+
+# # if True:
+# #  print("X")
+# #  print("Y")
+
+# #  print("A")
+# # if True:
+# #     print("B")
+# #     print("C")
+
+
+# # if True:
+# #     print("Yes")
+# #     print("No")
+
+
+# # print("Hello")
+# # print("World")
+# # # print("World")
+
+
+# print("A")
+
+# # print("B")
+
+# print("C")
+
+
+# print("X")
+# # '''
+# # print("Y")
+# # print("Z")
+# # '''
+# print("Z")
+
+
+# print("START")
+# if False:print("MIDDLE")
+# print("END")
+
+
+
+# for = 10
+    
+# number  = 10
+# Pratik_mishar = "pratik"
+
+# pratik1_pk = 1
+# 1pratk = 1
+
+# for = "Loop"
