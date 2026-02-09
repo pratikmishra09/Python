c3e9e424ca53488186195499d8b1dcb23a699e8c
Pratik Mishra <pk3057765@gmail.com>
2026-02-09 12:16:51 +0530

    Day9 complet in Python

---
+++ Day9/tuples.py
@@ -0,0 +1,13 @@
+tup = (1,23, 34,68,True,"pratik")
+# tup[0]=2
+print(type(tup),tup)
+tup[0]
+tup[-1]
+print(tup[0])
+print(tup[-1])
+print(len(tup))
+print(tup.count(1))
+print(tup.index(1))
+name = "pratik" 
+age = 21
+print(f"My name is {name} and age is {age}")
